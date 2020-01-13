fc6079c539d830d5f809ef753cc9feb83fdfbd48
weijupeng <275526034@qq.com>
2020-01-13 14:07:28 +0800

    初始化

---
+++ src/com/wjp/demo/entity/Hello.java
@@ -0,0 +1,11 @@
+package com.wjp.demo.entity;
+
+/**
+ * @author wjp
+ * @date 2020/1/13 14:03
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
